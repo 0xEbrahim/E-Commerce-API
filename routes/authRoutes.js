75cf4896b93ca802d21404af6ff7d63df063b3d4
Ebrahim El-Sayed <elsayedebrahim603@gmail.com>
2023-10-21 20:33:29 +0300

    HTTP method edit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,7 +9,7 @@ router.get('/all-users',authMiddleware, isAdmin, getAllUsers)
 router.get('/:id',authMiddleware,isAdmin,getSingleUser);
 router.delete('/delete-user/:id',authMiddleware,isAdmin, deleteSingleUser);
 router.delete('/deleteMyAcount',authMiddleware,deleteMyAccount);
-router.patch('/updateProfile',authMiddleware, updateSingleUser)
+router.put('/updateProfile',authMiddleware, updateSingleUser)
 router.put('/block-user/:id',authMiddleware, isAdmin, blockUser);
 router.put('/unblock-user/:id',authMiddleware, isAdmin, unBlockUser)
 
